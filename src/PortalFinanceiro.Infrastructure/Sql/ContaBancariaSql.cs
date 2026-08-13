05e41ae7275bd38fd40b4ff29f761dc457661979
Thayllon Gomes Souza <thayllon.souza@iterative.com.br>
2026-08-13 20:22:27 -0300

    fix: ContaBancariaSql.ListarPorUsuario agora filtra Ativo=1

--- src/PortalFinanceiro.Infrastructure/Sql/ContaBancariaSql.cs
+++ src/PortalFinanceiro.Infrastructure/Sql/ContaBancariaSql.cs
@@ -5,7 +5,7 @@ internal static class ContaBancariaSql
     static string T => $"{SqlDialect.Current.SchemaPrefix}ContaBancaria";
     static string C => "Id, IdUsuario, Nome, Banco, Tipo, Ativo, DataCadastro, DataAlteracao";
     public static string ObterPorId => $"SELECT {C} FROM {T} WHERE Id = @Id";
-    public static string ListarPorUsuario => $"SELECT {C} FROM {T} WHERE IdUsuario = @IdUsuario ORDER BY Nome";
+    public static string ListarPorUsuario => $"SELECT {C} FROM {T} WHERE IdUsuario = @IdUsuario AND Ativo = 1 ORDER BY Nome";
     public static string ContarReceitas => $"SELECT COUNT(*) FROM {SqlDialect.Current.SchemaPrefix}Receita WHERE IdConta = @IdConta AND Ativo = 1";
     public static string ContarDespesas => $"SELECT COUNT(*) FROM {SqlDialect.Current.SchemaPrefix}Despesa WHERE IdConta = @IdConta AND Ativo = 1";
     public static string Inserir => $"INSERT INTO {T} ({C}) VALUES (@Id, @IdUsuario, @Nome, @Banco, @Tipo, @Ativo, @DataCadastro, @DataAlteracao)";
